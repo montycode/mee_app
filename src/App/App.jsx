06720f248f6e46e871a700bc22078065f0283257
Omar Montoya <43155049+imremain@users.noreply.github.com>
2021-02-03 15:41:12 -0800

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Router, Route, Link } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
 import { history } from '@/Helpers';
 import { authenticationService } from '@/Services';
 import { PrivateRoute } from '@/Components';
-import { Dashboard, Login, Home, Register, Booking, Payment, BookingDetails } from '@/Pages';
+import { Dashboard, Login, Home, Register, Booking, Payment, BookingDetails, Topic, Confirmation } from '@/Pages';
 
 import '../Assets/css/style.css'
 import 'tailwindcss/tailwind.css'
+
+const promise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 class App extends React.Component {
     constructor(props) {
@@ -28,11 +32,15 @@ class App extends React.Component {
                 <main className="h-full bg-gray-50">
                     <PrivateRoute exact path="/dashboard" component={ Dashboard } />
                     <PrivateRoute exact path="/booking" component={ Booking } />
-                    <PrivateRoute exact path="/payment" component={ Payment } />
+                    <Elements stripe={promise}>
+                        <PrivateRoute exact path="/payment" component={ Payment } />
+                    </Elements>
+                    <PrivateRoute exact path="/confirmation" component={ Confirmation } />
                     <PrivateRoute exact path="/details" component={ BookingDetails } />
-                    <Route path="/login" component={Login} />
-                    <Route exact path="/" component={Home} />
+                    <Route exact path="/topics/:id" component={Topic} />
                     <Route exact path="/register" component={Register} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/" component={Home} />
                 </main>
             </Router>
         );
