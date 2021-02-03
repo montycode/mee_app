9c5b131833cceaeaf814f9e6ab963bc3fe726db0
Omar Montoya <43155049+imremain@users.noreply.github.com>
2021-02-03 15:41:47 -0800

    Update index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -2,3 +2,6 @@ export * from './PrivateRoute';
 export * from './Header';
 export * from './Navbar';
 export * from './Footer';
+export * from './CheckoutForm';
+export * from './TopicList';
+export * from './TopicCard';
