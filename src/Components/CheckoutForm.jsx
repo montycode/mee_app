07bfa11988aa0dd2b3c921d02f86efbfecc19859
Omar Montoya <43155049+imremain@users.noreply.github.com>
2021-02-04 13:10:07 -0800

    Update CheckoutForm.jsx

--- src/Components/CheckoutForm.jsx
+++ src/Components/CheckoutForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react'
+import config from 'config'
 import { authenticationService } from '@/Services'
 import {
   CardElement,
@@ -20,7 +21,7 @@ export const CheckoutForm = () => {
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
     window
-      .fetch("/create-payment-intent", {
+      .fetch(`${config.apiUrl}/create-payment-intent`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -52,7 +53,7 @@ export const CheckoutForm = () => {
       }
     });
     if (payload.error) {
-      setError(`Payment failed ${payload.error.message}`);
+      setError(`Error de pago ${payload.error.message}`);
       setProcessing(false);
     } else {
       setError(null);
@@ -77,33 +78,37 @@ export const CheckoutForm = () => {
         <label className="text-gray-900 font-semibold">Datos de Pago</label>
         <CardElement id="card-element" className="relative block w-full p-2 border bg-gray-200 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm" onChange={handleChange} />
       </div>
-      <div className="p-4">
-        <Link to='/booking'
-          className="group w-full col-span-1 relative uppercase flex justify-center py-2 px-4 border border-black text-sm font-medium text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-3000">
-          Regresar
-        </Link>
-      </div>
-      <div className="p-4">
-        <button
-          disabled={processing || disabled || succeeded}
-          id="submit"
-          className="group w-full col-span-1 relative uppercase flex justify-center py-2 px-4 border border-black text-sm font-medium text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300"
-        >
-          <span id="button-text">
-            {processing ? (
-              <div className="spinner" id="spinner"></div>
-            ) : (
-              "Realizar Pago"
-            )}
-          </span>
-        </button>
-      </div>
-      {/* Show any error that happens when processing the payment */}
-      {error && (
-        <div className="card-error" role="alert">
-          {error}
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {/* Show any error that happens when processing the payment */}
+        {error && (
+        <div className="p-4 col-span-1 md:col-span-2">
+            <p className="text-center italic font-bold col-span-2 p-2 text-red-500">
+                {error}
+            </p>
         </div>
-      )}
+        )}
+        <div className="p-4 col-span-1">
+          <Link to='/booking'
+            className="group w-full col-span-1 relative uppercase flex justify-center py-2 px-4 border border-black text-sm font-medium text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-3000">
+            Regresar
+          </Link>
+        </div>
+        <div className="p-4 col-span-1">
+          <button
+            disabled={processing || disabled || succeeded}
+            id="submit"
+            className="group w-full col-span-1 relative uppercase flex justify-center py-2 px-4 border border-black text-sm font-medium text-black bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300"
+          >
+            <span id="button-text">
+              {processing ? (
+                <div className="spinner" id="spinner"></div>
+              ) : (
+                "Realizar Pago"
+              )}
+            </span>
+          </button>
+        </div>
+      </div>
       {/* Show a success message upon completion */}
       <p className={succeeded ? "result-message" : "result-message hidden"}>
         Payment succeeded, see the result in your
